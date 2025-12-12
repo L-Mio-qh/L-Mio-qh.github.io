e1cfd7e64c7d46f5b0ef17539463b57f1c95d66f
lin yimin <2859594289@qq.com>
2025-12-12 13:25:28 +0800

    Site updated: 2025-12-12 13:25:27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1765455422722";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1765517109844";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
