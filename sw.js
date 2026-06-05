46d1d7e7d9aec46e04bfff88aa99c79c67095f4a
L-Mio-qh <2859594289@qq.com>
2026-06-05 11:38:57 +0800

    Site updated: 2026-06-05 11:38:56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1779972595458";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1780630734403";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
